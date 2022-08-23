e8e37e0ac519574f80a2f257af67f02896332477
elbasel <abdelrahman.elbasel@gmail.com>
2022-08-23 14:52:40 +0200

    added colors to template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
+  mode: "jit",
+  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
+    colors: {
+      colors,
+    },
     extend: {},
   },
   plugins: [],
